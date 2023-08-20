92378c4c241ddccf7f4305416ca9e177bbccbf1c
Atikul islam <140721407+Atikxyz@users.noreply.github.com>
2023-08-20 17:15:38 +0600

    Add files via upload

---
+++ js/some.js
@@ -0,0 +1,37 @@
+// header-mobile-menu
+function showMenu() {
+  document.getElementById('navLink').style.width = '50%';
+}
+
+function hideMenu() {
+  document.getElementById('navLink').style.width = '0';
+}
+
+// hero-card-slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("card");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
